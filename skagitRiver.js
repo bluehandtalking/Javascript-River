2ca610e8c04b21af180628ccbad689ec7033eae6
Jerrold Thompson <jet@whidbey.com>
2016-05-08 18:18:36 -0700

    fix iterator for isRiver

--- skagitRiver.js
+++ skagitRiver.js
@@ -2,18 +2,14 @@
 //  JS Object literal, and prototype example
 
 
-function isRiver(element, index, array) {
-  return (element.name == "Skagit");
-}
-
 // Constructor function for river
-function River( name, location, length, source, estuary) {
-   if(name  === "" || location === "" || length  === "" || source === "" || estuary === "") {
-      throw "No name or location or length or source or estuary";
+function River( name, riverLocation, length, source, estuary) {
+   if(name  === "" || riverLocation === "" || length  === "" || source === "" || estuary === "") {
+      throw "No name or riverLocation or length or source or estuary";
    }
    else {
       this.name = name;
-      this.location = location;
+      this.riverLocation = riverLocation;
       this.length = length;
       this.source = source;
       this.estuary = estuary;
@@ -22,14 +18,22 @@ function River( name, location, length, source, estuary) {
 
 
 River.prototype.toString = function () {
-   return "River: " + this.name + ", " + this.location + ", " + this.length + ", " + this.source + ", " + this.estuary
+   return "River: " + this.name + ", " + this.riverLocation + ", " + this.length + ", " + this.source + ", " + this.estuary
 };
 
 var myRivers = (function () {
   // private
   var rivers = [];
-  getRiver = function(r) {
-    return rivers[r];
+  var isRiver = function(r) {
+    for(var idx=0; idx <= (rivers.length -1); idx++) { 
+        if(r == rivers[idx].name)
+          return idx;
+    }
+  }
+  var getRiver = function(r) {
+    var river = rivers[r]
+    var result = river.toString();
+    return result;
   }; 
   // public API
   return {
@@ -43,21 +47,28 @@ var myRivers = (function () {
       }
       return allRivers;
     },
-    getRiverStats: function() {
-      var result = rivers.filter(isRiver)
-        console.log(result)
+    getRiverStats: function(r) {
+      var idx;
+      idx = isRiver(r)
+      if (idx != null )
+        return getRiver(idx);
+      else
+        return (r + " is not a recorded river")
     }	 
   };
 })();
 
 //  Test
 
-myRivers.riverNew(new River("Skagit", "S.W. British Columbia / N.W. Washington", "150 miles", "Allison Pass", "Skagit Bay"));
-
+myRivers.riverNew(new River("Skagit", "S.W. British Columbia  N.W. Washington", "150 miles", "Allison Pass", "Skagit Bay"));
 myRivers.riverNew(new River("Stillaguamish", "NorthWest Washington", "67 miles", "North Cascades", "Port Susan"));
-
-myRivers.riverNew(new River("Nisqually", "Central West Washington", "81  miles", "Nisqually Glacier", "Puget Sound"));
-
+myRivers.riverNew(new River("Nooksack", "Whatcom County", "68 miles", "North Cascades", "Skagit Bay") );
+myRivers.riverNew(new River("Snohomish", "Central West Washington", "81  miles", "Nisqually Glacier", "Puget Sound"));
+myRivers.riverNew(new River("Green", "King County", "98 miles", "Central Cascades", "Puget Sound"))
 console.log(myRivers.getRivers());
-console.log(myRivers.getRiverStats("Nisqually"));
-
+console.log(myRivers.getRiverStats("Snake"));
+console.log(myRivers.getRiverStats("Nooksack"));
+console.log(myRivers.getRiverStats("Stillaguamish"));
+console.log(myRivers.getRiverStats("Skagit"));
+console.log(myRivers.getRiverStats("Snohomish"));
+console.log( myRivers.getRiverStats("Green") );
